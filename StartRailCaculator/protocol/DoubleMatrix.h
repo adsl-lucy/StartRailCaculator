d8ebbb203666b88d6d02819a23dfb04392b4a879
adsl_lucy <416322045@qq.com>
2023-10-31 18:46:25 +0800

    Update DoubleMatrix.h

--- StartRailCaculator/protocol/DoubleMatrix.h
+++ StartRailCaculator/protocol/DoubleMatrix.h
@@ -24,10 +24,10 @@
 #include <QVector>
 #include <QSize>
 
-//double���󣬷�����ж�ά�������
-//�������ʱĬ����Ԫ��ֵΪ0
-//������±��������ϰ�ߣ���һ��Ԫ���±�Ϊ0������ζ�����Ͻ�Ԫ�ص��±�Ϊ��0��0����
-//�������ĳߴ���3��4�У���ô���½�Ԫ�ص��±�Ϊ��2��3��
+//double矩阵，方便进行二维数阵操作
+//扩充矩阵时默认新元素值为0
+//矩阵的下标遵从数组习惯，第一个元素下标为0（这意味着左上角元素的下标为（0，0））
+//如果矩阵的尺寸是3行4列，那么右下角元素的下标为（2，3）
 
 
 class DoubleMatrix
@@ -37,32 +37,32 @@ public:
 	DoubleMatrix(QSize size);
 	~DoubleMatrix();
 
-	//���ؾ����С,����QSize��height����������width��������
-	const QSize getMatrixSize() const { return QSize(m_column, m_row); }
-	//���ָ��Ԫ���Ƿ����
+	//返回矩阵大小,这里QSize的height就是行数，width就是列数
+	const QSize 	getMatrixSize() const { return QSize(m_column, m_row); }
+	//检查指定元素是否存在
 	bool		checkElementExist(int row, int column);
-	//�����ض�λ�õ�ֵ���������MatrixSize����ô�᷵��-1
+	//返回特定位置的值，如果超出MatrixSize，那么会返回-1
 	double		getValueAt(int row, int column);
-	//�����ض�λ�õ�ֵ�������������MatrixSize�������������֮ǰ���ߴ�
-	const double getConstValueAt(int row, int column) const { return m_VecMatrixValue[row][column];};
-	//�����ض�λ�õ�ֵ���ã�������Χ���
+	//返回特定位置的值常量，如果超出MatrixSize会崩溃，建议用之前检查尺寸
+	const double 	getConstValueAt(int row, int column) const { return m_VecMatrixValue[row][column];};
+	//返回特定位置的值引用，超出范围会崩
 	double&		getValueRefAt(int row, int column);
-	//���þ���һ��λ�õ�ֵ�������������Χ����ô�Զ�������󵽰�����Ԫ����С�ߴ�
+	//设置矩阵一个位置的值，如果超出矩阵范围，那么自动扩充矩阵到包含该元素最小尺寸
 	void		setValue(int row, int column,double value);
-	//���ž����С����֤�������ź�ĳߴ�������row�к�column��
-	//������������/����С�ڵ�ǰ�ߴ磬��ô��/�в����в���
+	//扩张矩阵大小，保证矩阵扩张后的尺寸至少有row行和column列
+	//如果输入的行数/列数小于当前尺寸，那么行/列不会有操作
 	void		expandMatrix(int row, int column);
 
 	void		expandMatrix(QSize size) { expandMatrix(size.height(),size.width()); };
-	//��������
+	//清理矩阵
 	void		clearMatrix();
 
 private:
 
-	int m_row		= 0;//����������ֵΪ4����ζ��һ����4�У����Ǵ�ʱ�������±���3
-	int m_column	= 0;//����������ֵΪ3����ζ��һ����3�У����Ǵ�ʱ�������±���2
+	int m_row	= 0;//矩阵行数，值为4，意味着一共有4行，但是此时最大的行下标是3
+	int m_column	= 0;//矩阵列数，值为3，意味着一共有3列，但是此时最大的列下标是2
 	
-	//�������������������ϻᱣ֤ÿһ�еĶ�Ӧ�������ǵȳ���
+	//矩阵容器，代码理论上会保证每一行的对应的数组是等长的
 	QVector<QVector<double>> m_VecMatrixValue;
 
 };
